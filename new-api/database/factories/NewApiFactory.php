bdeecee75e80e8340374462bee680327e346b318
Matheus Patricio da S e Silva <106037069+MatheusPatricioo@users.noreply.github.com>
2024-11-08 11:32:11 -0300

    update

---
+++ new-api/database/factories/NewApiFactory.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\User;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\NewApi>
+ */
+class NewApiFactory extends Factory
+{
+    /**
+     * Defina o estado padrão do modelo.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        $paid = $this->faker->boolean(); // Valor booleano aleatório (verdadeiro ou falso)
+
+        return [
+            'user_id' => User::all()->random()->id,
+            'type' => $this->faker->randomElement(['B','C','P']),
+            'paid' => $paid,
+            'value'=> $this->faker->numberBetween(1000, 10000),
+            'payment_date' => $paid ? $this->faker->dateTimeThisMonth() : null, // Data de pagamento condicional
+        ];
+    }
+}
