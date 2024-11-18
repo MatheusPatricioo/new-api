de32092ae676c582a1b1e479a54f161869206822
Matheus Patricio da S e Silva <106037069+MatheusPatricioo@users.noreply.github.com>
2024-11-18 11:15:52 -0300

    up

--- new-api/app/Http/Resources/V1/InvoiceResource.php
+++ new-api/app/Http/Resources/V1/InvoiceResource.php
@@ -2,22 +2,30 @@
 
 namespace App\Http\Resources\v1;
 
+use Carbon\Carbon;
 use Illuminate\Http\Request;
 use Illuminate\Http\Resources\Json\JsonResource;
 
 class InvoiceResource extends JsonResource
 {
+    private array $types = ['C' => 'Cartão', 'B' => 'Boleto', 'P' => 'Pix']; // Tipos de pagamentos
 
-    private array $types = ['C'=>'cartão', 'B' =>'Boleto', 'P' =>'Pix']; // tipos de pagamentos :)
     public function toArray(Request $request): array
     {
+        $paid = $this->paid;
         return [
-            'user'=> [
+            'user' => [
                 'firstName' => $this->user->firstName,
                 'lastName' => $this->user->lastName,
                 'fullName' => $this->user->firstName . ' ' . $this->user->lastName,
                 'email' => $this->user->email,
-            ]
+            ],
+            'type' => $this->types[$this->type] ?? 'Tipo desconhecido', // Garantir que não quebre
+            'value' => 'R$ ' . number_format($this->value, 2, ',', '.'),
+            'paid' => $paid ? 'Pago' : 'Não pago',
+            'payment_date' => $paid
+                ? Carbon::parse($this->payment_date)->format('d/m/Y H:i:s')
+                : 'Não disponível',
         ];
     }
 }
